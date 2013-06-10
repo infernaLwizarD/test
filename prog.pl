4bb65dc67dac4e7c6a8ccce82dad68fc62899b0d
infernal <nikita.dolgopolov@gmail.com>
2013-06-11 00:56:02 +0300

    commit 1

---
+++ prog.pl
@@ -0,0 +1,18 @@
+#!usr/bin/perl
+use strict;
+use LWP::UserAgent;
+
+my $ua = LWP::UserAgent->new;
+$ua->agent('Mozilla/5.0');
+my $request = HTTP::Request->new(GET => 'http://bash.im');
+$request->header('Accept' => 'text/html');
+my $response = $ua->request($request);
+
+my @content = split "\n", $response->content;
+my @quotes = map />(.*)</, (grep /class="text"/, @content);
+open FILE, ">file.txt";
+print FILE "[";
+foreach (@quotes) {
+	print FILE "\n\t{\n\t\t" . '"text" : "' . $_ . "\"\n\t}" . (($quotes[$#quotes] eq $_) ? "\n" : ",\n");
+}
+print FILE "]";
