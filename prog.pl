cc0b0fc8b992a664c7e12d58ea77cc57a0373de7
infernal <nikita.dolgopolov@gmail.com>
2013-06-11 16:45:15 +0300

    commit 7

--- prog.pl
+++ prog.pl
@@ -1,18 +1,21 @@
 #!usr/bin/perl
 use strict;
+#use Data::Dumper;
 use LWP::UserAgent;
 
 my $ua = LWP::UserAgent->new;
 $ua->agent('Mozilla/5.0');
-my $request = HTTP::Request->new(GET => 'http://bash.im');
-$request->header('Accept' => 'text/html');
-my $response = $ua->request($request);
+my $response = $ua->get('http://bash.im');
 
 my @content = split "\n", $response->content;
 my @quotes = map />(.*)</, (grep /class="text"/, @content);
 
-print "[";
-foreach (@quotes) {
-	print "\n\t{\n\t\t\"text\" : \"" . $_ . "\"\n\t}" . (($quotes[$#quotes] eq $_) ? "\n" : ",\n");
+if ($response->is_success) {
+	print "[";
+	foreach (@quotes) {
+		print "\n\t{\n\t\t\"text\" : \"" . $_ . "\"\n\t}" . (($quotes[$#quotes] eq $_) ? "\n" : ",\n");
+	}
+	print "]";
+} else {
+	print "Error: " . $response->status_line . "\n";
 }
-print "]";
