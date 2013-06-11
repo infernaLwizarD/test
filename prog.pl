dc84dae55ba014883794fb7cf9a10a2e83f28163
infernal <nikita.dolgopolov@gmail.com>
2013-06-11 16:49:55 +0300

    commit 8

--- prog.pl
+++ prog.pl
@@ -4,24 +4,26 @@ use LWP::UserAgent;
 
 my $ua = LWP::UserAgent->new;
 $ua->agent('Mozilla/5.0');
-my $request = HTTP::Request->new(GET => 'http://bash.im');
-$request->header('Accept' => 'text/html');
-my $response = $ua->request($request);
+my $response = $ua->get('http://bash.im');
 
-my @content = split "\n", $response->content;
-my (@numbers, @quotes);
-foreach (@content) {
-	if (/class="id"/) {
-		/>#(.*)<\/a/;
-		push @numbers, $1;
-	} elsif (/class="text"/) {
-		/>(.*)</;
-		push @quotes, $1;
+if ($response->is_success) {
+	my @content = split "\n", $response->content;
+	my (@numbers, @quotes);
+	foreach (@content) {
+		if (/class="id"/) {
+			/>#(.*)<\/a/;
+			push @numbers, $1;
+		} elsif (/class="text"/) {
+			/>(.*)</;
+			push @quotes, $1;
+		}
 	}
-}
 
-print "[";
-foreach (0..$#numbers) {
-	print "\n\t{\n\t\t\"number\" : $numbers[$_],\n\t\t\"text\" : \"" . $quotes[$_] . "\"\n\t}" . (($quotes[$#quotes] eq $quotes[$_]) ? "\n" : ",\n");
+	print "[";
+	foreach (0..$#numbers) {
+		print "\n\t{\n\t\t\"number\" : $numbers[$_],\n\t\t\"text\" : \"" . $quotes[$_] . "\"\n\t}" . (($quotes[$#quotes] eq $quotes[$_]) ? "\n" : ",\n");
+	}
+	print "]";
+} else {
+	print "Error: " . $response->status_line . "\n";
 }
-print "]";
